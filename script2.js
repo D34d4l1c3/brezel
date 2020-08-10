d248630c1da929142a6d9c32ea5e98363f32516c
D34d4l1c3 <D34d4l1c3@gmail.com>
2020-08-10 21:28:41 +0300

    Работа с элементами списка + правки

--- script2.js
+++ script2.js
@@ -1,18 +1,42 @@
-const produktInfoName = document.querySelector('#produkt__info-name');
-const produktInfoMasse = document.querySelector('#produkt__info-masse');
-const produktInfoPreis = document.querySelector('#produkt__info-preis');
+const elProductList  = document.querySelector('#produсt__info-name');
+const elProductMass = document.querySelector('#produсt__info-masse');   //Mass
+const elProductPrice = document.querySelector('#produсt__info-preis');  //Price 
+console.log(elProductList);
 
 for(let i = 0; i < 3; i++) {
-       const produktInfoName = prompt('Укажите название любимого изделия', ''),
-         produktInfoMasse = +prompt('Укажите вес изделия в граммах', ''),
-         produktInfoPreis = +prompt('Укажите приемлемую стоимость изделия в рублях', '');
+       const produсtInfoName = prompt('Укажите название любимого изделия №'+(i+1), ''),
+         produсtInfoMasse = +prompt('Укажите вес изделия в граммах'+(i+1), ''),
+         produсtInfoPreis = +prompt('Укажите приемлемую стоимость изделия в рублях'+(i+1), '');
 
-    if(produktInfoName != null && produktInfoMasse != null && produktInfoPreis != null && 
-        produktInfoName != '' && produktInfoMasse != '' && produktInfoPreis != '' && produktInfoName.length < 60) {
-        console.log(produktInfoName);
-        console.log(produktInfoMasse);
-        console.log(produktInfoPreis);
+    if(produсtInfoName != null && produсtInfoMasse != null && produсtInfoPreis != null && 
+        produсtInfoName != '' && produсtInfoMasse != '' && produсtInfoPreis != '' && produсtInfoName.length < 60) { 
+        console.log(produсtInfoName);
+        console.log(produсtInfoMasse);
+        console.log(produсtInfoPreis);
         console.log('Yes');
+        if(i == 0){
+            elProductList.innerText     = produсtInfoName;
+            elProductMass.innerText     = produсtInfoMasse;
+            elProductPrice.innerText    = produсtInfoPreis;
+        } else {
+            let list = document.createElement("ul");
+            list.classList.add("produсt__list");
+            list.append("Список");
+            let listEl1 = document.createElement("li");
+            listEl1.classList.add("produсt__info"+i);
+            listEl1.innerText = produсtInfoName;
+            let listEl2 = document.createElement("li");
+            listEl2.classList.add("produсt__info"+i);
+            listEl2.innerText = produсtInfoMasse;
+            let listEl3 = document.createElement("li");
+            listEl3.classList.add("produсt__info"+i);
+            listEl3.innerText = produсtInfoName;
+            list.appendChild(listEl1);
+            list.appendChild(listEl2);
+            list.appendChild(listEl3);
+            document.querySelector('.produсt').appendChild(list);
+        }
+        
     } else{
         console.log('Error');
         i--;
