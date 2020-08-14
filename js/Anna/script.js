5062e8079490f13a6f94f87f16a6e9b01a688415
D34d4l1c3 <D34d4l1c3@gmail.com>
2020-08-14 20:22:16 +0300

    Изменения по проекту

--- js/Anna/script.js
+++ js/Anna/script.js
@@ -4,7 +4,10 @@ let productName = '',   //название кондитерского издел
     discount = 5;       //размер скидки Number(!)
     check = true;//Идентификатор правильности вводы, если false то цикл останавливает итерацию пока не введет правильно, выйти из цикла можно только по ответу на вопрос а хочет ли продолжить - Отмена
     
-for (let i = 0;i<2;i = check ? ++i : i){ 
+for (let i = 0;i<2;i = check ? i++ : i){ 
+    let n = 0;
+    n = n++;
+    console.log(n);
     productName = prompt(`За какое ${i + 1}-e кондитерское изделие Вы бы хотели скидку?`,"");
     check = productName ? true : false; //Не сравниваю с !="" потому что если ничего не ответят и скажут ок будет пусто а это "" == false, если  нажмут отмена то будет null == false, и только если что-то введут будет true
     if (check){
@@ -28,10 +31,10 @@ for (let i = 0;i<2;i = check ? ++i : i){
             else break;
         }   
     } else {
-        let table = document.getElementById('table_product');
-        let row = table.insertRow(i+1);
-        let cell1 = row.insertCell(0);
-        let cell2 = row.insertCell(1);
+        let table   = document.getElementById('table_product');
+        let row     = table.insertRow(i+1);
+        let cell1   = row.insertCell(0);
+        let cell2   = row.insertCell(1);
         cell1.innerHTML = productName;
         cell2.innerHTML = discount+"%";
         console.log(`Название ${i + 1}-го продукта ${productName}`);  
